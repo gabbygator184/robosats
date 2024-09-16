beafab004895513f60bb8185424857f149f4c356
koalasat <koalasat@satstralia.com>
2024-09-16 12:03:29 +0200

    Parse nostr note

--- frontend/src/contexts/FederationContext.tsx
+++ frontend/src/contexts/FederationContext.tsx
@@ -15,8 +15,6 @@ import { federationLottery } from '../utils';
 import { AppContext, type UseAppStoreType } from './AppContext';
 import { GarageContext, type UseGarageStoreType } from './GarageContext';
 import { type Origin, type Origins } from '../models/Coordinator.model';
-import { SimplePool } from 'nostr-tools';
-import { SubCloser } from 'nostr-tools/lib/types/abstract-pool';
 
 export interface CurrentOrderIdProps {
   id: number | null;
@@ -58,9 +56,6 @@ export const FederationContextProvider = ({
   );
   const [federationUpdatedAt, setFederationUpdatedAt] = useState<string>(new Date().toISOString());
 
-  const [relayPool] = useState<SimplePool>(new SimplePool());
-  const [relayConnection, setRelayConnection] = useState<SubCloser>();
-
   useEffect(() => {
     setMaker((maker) => {
       return { ...maker, coordinator: sortedCoordinators[0] };
@@ -68,32 +63,12 @@ export const FederationContextProvider = ({
     federation.registerHook('onFederationUpdate', () => {
       setFederationUpdatedAt(new Date().toISOString());
     });
-
-    setRelayConnection(
-      relayPool.subscribeMany(
-        ['ws://satstraoq35jffvkgpfoqld32nzw2siuvowanruindbfojowpwsjdgad.onion/nostr'],
-        [
-          {
-            authors: ['116bf3493edd2136f5e0c6a11b6c24d0eb34bbd5bf8ede965a9548726de710c7'],
-            kinds: [38383],
-            '#s': ['pending'],
-          },
-        ],
-        {
-          onevent(event) {
-            console.log(event);
-          },
-          oneose() {
-            relayConnection?.close();
-          },
-        },
-      ),
-    );
   }, []);
 
   useEffect(() => {
     if (client !== 'mobile' || torStatus === 'ON' || !settings.useProxy) {
       void federation.updateUrl(origin, settings, hostUrl);
+      void federation.connectNostr();
       void federation.update();
     }
   }, [settings.network, settings.useProxy, torStatus]);
--- frontend/src/models/Federation.model.ts
+++ frontend/src/models/Federation.model.ts
@@ -1,3 +1,5 @@
+import { SimplePool } from 'nostr-tools';
+import Geohash from 'latlon-geohash';
 import {
   Coordinator,
   type Exchange,
@@ -7,10 +9,12 @@ import {
   defaultExchange,
 } from '.';
 import defaultFederation from '../../static/federation.json';
+import currencyDict from '../../static/assets/currencies.json';
 import { systemClient } from '../services/System';
 import { getHost } from '../utils';
 import { coordinatorDefaultValues } from './Coordinator.model';
 import { updateExchangeInfo } from './Exchange.model';
+import { fromUnixTime } from 'date-fns';
 
 type FederationHooks = 'onFederationUpdate';
 
@@ -33,7 +37,7 @@ export class Federation {
       ...defaultExchange,
       totalCoordinators: Object.keys(this.coordinators).length,
     };
-    this.book = [];
+    this.book = {};
     this.hooks = {
       onFederationUpdate: [],
     };
@@ -59,10 +63,132 @@ export class Federation {
 
   public coordinators: Record<string, Coordinator>;
   public exchange: Exchange;
-  public book: PublicOrder[];
+  public book: Record<string, PublicOrder>;
   public loading: boolean;
 
   public hooks: Record<FederationHooks, Array<() => void>>;
+
+  public relayPool: SimplePool = new SimplePool();
+
+  connectNostr = (): void => {
+    this.loading = true;
+    this.book = {};
+
+    const relays = ['ws://satstraoq35jffvkgpfoqld32nzw2siuvowanruindbfojowpwsjdgad.onion/nostr'];
+
+    this.exchange.loadingCoordinators = relays.length;
+
+    this.relayPool.trustedRelayURLs = new Set<string>(relays);
+    this.relayPool.subscribeMany(
+      relays,
+      [
+        {
+          authors: Object.values(defaultFederation)
+            .map((f) => f.nostrHexPubkey)
+            .filter((item) => item !== undefined),
+          kinds: [38383],
+          '#s': ['pending'],
+          '#n': ['mainnet'],
+        },
+      ],
+      {
+        onevent: (event) => {
+          const publicOrder: PublicOrder = {
+            id: 0,
+            coordinatorShortAlias: '',
+            created_at: new Date(),
+            expires_at: new Date(),
+            type: 1,
+            currency: null,
+            amount: '',
+            has_range: false,
+            min_amount: null,
+            max_amount: null,
+            payment_method: '',
+            is_explicit: false,
+            premium: '',
+            satoshis: null,
+            maker: null,
+            escrow_duration: 10800, // FIXME
+            bond_size: '',
+            latitude: null,
+            longitude: null,
+            maker_nick: '',
+            maker_hash_id: '8a928ef7b30bd07a4bf2493bee55e9b3a00c4962be0a743649b7ac2f0e031c4e', // FIXME
+            satoshis_now: null, // FIXME
+            price: null, // FIXME
+          };
+          let dTag = '';
+          event.tags.forEach((tag) => {
+            switch (tag[0]) {
+              case 'd':
+                dTag = tag[1];
+                break;
+              case 'k':
+                publicOrder.type = tag[1] === 'sell' ? 1 : 0;
+                break;
+              case 'expiration':
+                publicOrder.expires_at = fromUnixTime(parseInt(tag[1], 10));
+                break;
+              case 'fa':
+                if (tag[2]) {
+                  publicOrder.has_range = true;
+                  publicOrder.min_amount = tag[1] ?? null;
+                  publicOrder.max_amount = tag[2] ?? null;
+                } else {
+                  publicOrder.amount = tag[1];
+                }
+                break;
+              case 'bond':
+                publicOrder.bond_size = tag[1];
+                break;
+              case 'name':
+                publicOrder.maker_nick = tag[1];
+                break;
+              case 'premium':
+                publicOrder.premium = tag[1];
+                break;
+              case 'pm':
+                tag.shift();
+                publicOrder.payment_method = tag.join(' ');
+                break;
+              case 'g':
+                const { lat, lon } = Geohash.decode(tag[1]);
+                publicOrder.latitude = lat;
+                publicOrder.longitude = lon;
+                break;
+              case 'f':
+                const currencyNumber = Object.entries(currencyDict).find(
+                  ([_key, value]) => value === tag[1],
+                );
+                publicOrder.currency = currencyNumber?.[0] ? parseInt(currencyNumber[0], 10) : null;
+                break;
+              case 'source':
+                const orderUrl = tag[1].split('/');
+                publicOrder.id = parseInt(orderUrl[orderUrl.length - 1] ?? '0');
+                const coordinatorIdentifier = orderUrl[orderUrl.length - 2] ?? '';
+                publicOrder.coordinatorShortAlias = Object.entries(defaultFederation).find(
+                  ([key, value]) => value.identifier === coordinatorIdentifier,
+                )?.[0];
+                break;
+              default:
+                break;
+            }
+          });
+
+          // price = limitsList[index].price * (1 + premium / 100);
+
+          this.book[dTag] = publicOrder;
+        },
+        oneose: () => {
+          this.exchange.loadingCoordinators = this.exchange.loadingCoordinators - 1;
+          this.loading = this.exchange.loadingCoordinators > 0;
+          this.updateExchange();
+          this.triggerHook('onFederationUpdate');
+        },
+      },
+    );
+  };
 
   addCoordinator = (
     origin: Origin,
@@ -92,9 +218,12 @@ export class Federation {
   };
 
   onCoordinatorSaved = (): void => {
-    this.book = Object.values(this.coordinators).reduce<PublicOrder[]>((array, coordinator) => {
-      return [...array, ...coordinator.book];
-    }, []);
+    this.book = Object.values(this.coordinators).reduce<Record<string, PublicOrder>>(
+      (book, coordinator) => {
+        return { ...book, ...coordinator.book };
+      },
+      {},
+    );
     this.exchange.loadingCoordinators =
       this.exchange.loadingCoordinators < 1 ? 0 : this.exchange.loadingCoordinators - 1;
     this.loading = this.exchange.loadingCoordinators > 0;
@@ -126,25 +255,25 @@ export class Federation {
     this.exchange.onlineCoordinators = 0;
     this.exchange.loadingCoordinators = Object.keys(this.coordinators).length;
     this.updateEnabledCoordinators();
-    for (const coor of Object.values(this.coordinators)) {
-      void coor.update(() => {
-        this.exchange.onlineCoordinators = this.exchange.onlineCoordinators + 1;
-        this.onCoordinatorSaved();
-      });
-    }
+    // for (const coor of Object.values(this.coordinators)) {
+    //   void coor.update(() => {
+    //     this.exchange.onlineCoordinators = this.exchange.onlineCoordinators + 1;
+    //     this.onCoordinatorSaved();
+    //   });
+    // }
   };
 
   updateBook = async (): Promise<void> => {
-    this.loading = true;
-    this.book = [];
-    this.triggerHook('onFederationUpdate');
-    this.exchange.loadingCoordinators = Object.keys(this.coordinators).length;
-    for (const coor of Object.values(this.coordinators)) {
-      void coor.updateBook(() => {
-        this.onCoordinatorSaved();
-        this.triggerHook('onFederationUpdate');
-      });
-    }
+    // this.loading = true;
+    // this.book = [];
+    // this.triggerHook('onFederationUpdate');
+    // this.exchange.loadingCoordinators = Object.keys(this.coordinators).length;
+    // for (const coor of Object.values(this.coordinators)) {
+    //   void coor.updateBook(() => {
+    //     this.onCoordinatorSaved();
+    //     this.triggerHook('onFederationUpdate');
+    //   });
+    // }
   };
 
   updateExchange = (): void => {
